ede6b7974281d9a9c9f7ae29045a3182b327e70e
Saruman <evillarreal@smd.com.pa>
2015-06-01 10:55:25 -0500

    Initial commit

---
+++ vendor/autoload.php
@@ -0,0 +1,30 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+spl_autoload_register(function ($class)
+{
+  // project-specific namespace prefix
+  $prefix = 'libphonenumber\\';
+
+  // base directory for the namespace prefix
+  $base_dir = defined('LIBPHONENUMBER_SRC_DIR') ? LIBPHONENUMBER_SRC_DIR : __DIR__ . '/libphonenumber-for-php/src/libphonenumber/';
+
+  // does the class use the namespace prefix?
+  $len = strlen($prefix);
+  if (strncmp($prefix, $class, $len) !== 0) {
+    // no, move to the next registered autoloader
+    return;
+  }
+
+  // get the relative class name
+  $relative_class = substr($class, $len);
+
+  // replace the namespace prefix with the base directory, replace namespace
+  // separators with directory separators in the relative class name, append
+  // with .php
+  $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+  // if the file exists, require it
+  if (file_exists($file)) {
+    require $file;
+  }
+});
